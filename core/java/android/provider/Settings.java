1096b0eaba32d1307725afffcdbd376dd79312f3
ezio84 <brabus84@gmail.com>
2016-10-15 16:31:33 +0200

    Fw_base - QS BT easy toggle: if enabled, long press for detail view
    Change-Id: I06dd4aaf82de6391864111aab52032efb50ea81c
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3744,6 +3744,12 @@ public final class Settings {
         public static final String QS_WIFI_EASY_TOGGLE = "qs_wifi_easy_toggle";
 
         /**
+         * Enable single click to turn Bluetooth on or off.
+         * @hide
+         */
+        public static final String QS_BT_EASY_TOGGLE = "qs_bt_easy_toggle";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -6171,6 +6177,7 @@ public final class Settings {
 
         /**
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
          * Enable single click to turn WiFi on or off.
          * @hide
@@ -6178,13 +6185,18 @@ public final class Settings {
         public static final String QS_WIFI_EASY_TOGGLE = "qs_wifi_easy_toggle";
 
         /**
+=======
+>>>>>>> db57285a8e0... Fw_base - QS BT easy toggle: if enabled, long press for detail view
          * Enable single click to turn Bluetooth on or off.
          * @hide
          */
         public static final String QS_BT_EASY_TOGGLE = "qs_bt_easy_toggle";
 
         /**
+<<<<<<< HEAD
 >>>>>>> 5bdd7732433... Fw_base - QS WiFi easy toggle: if enabled, long press for detail view
+=======
+>>>>>>> db57285a8e0... Fw_base - QS BT easy toggle: if enabled, long press for detail view
          * Whether the device should wake when the wake gesture sensor detects motion.
          * @hide
          */
