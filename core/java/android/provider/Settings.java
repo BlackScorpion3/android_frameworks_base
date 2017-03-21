fb803a60b6bec5464e577a8e3893aa324abbd563
blackscorpion3 <don.berzley@gmail.com>
2017-03-21 00:14:56 -0400

    Revert " Frameworks: Statusbar BatteryBar (1/2)"
    This reverts commit b212cf57363489f729b90ed28484774d3ac8370e.
    
    Change-Id: Ia637c08cf3866fa3689ac835b584d09cb33f6a81

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3762,30 +3762,6 @@ public final class Settings {
         public static final String QS_SHOW_BRIGHTNESS_ICON = "qs_show_brightness_icon";
 
         /**
-         * Whether to show the battery bar
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
-
-        /**
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
-
-        /**
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
-
-        /**
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
-
-        /**
-         * @hide
-         */
-        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
 
         /**
          * Colors used for the color picker panel puttons
