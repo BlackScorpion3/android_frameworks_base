90fa511e49c29296643182916863357da4d0c390
blackscorpion3 <don.berzley@gmail.com>
2017-03-20 21:50:04 -0400

    Move helpers to king folder
    Change-Id: I37896a72bcffcdd4540392b9674ce3eeb714aa74

--- core/java/com/android/internal/util/king/ColorHelper.java
+++ core/java/com/android/internal/util/king/ColorHelper.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2013-2015 RR
+* Copyright (C) 2017 King Rom
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 * limitations under the License.
 */
 
-package com.android.internal.util.rr;
+package com.android.internal.util.king;
 
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
